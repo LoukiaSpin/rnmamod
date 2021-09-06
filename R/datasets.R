c032f8ed003ee1178bced8f5497c2067ea144775
LoukiaSpin <spineli.loukia@mh-hannover.de>
2021-09-06 12:30:36 +0200

    Correct treatment name

--- R/datasets.R
+++ R/datasets.R
@@ -5,7 +5,7 @@
 #'
 #' @format A data-frame with 21 rows of arm-based data and 17 columns (maximum number of 4 arms).
 #'
-#' @details The interventions have been coded as follows: 1, placebo; 2, budesodine; 3, budesodine plus formoterol;
+#' @details The interventions have been coded as follows: 1, placebo; 2, budesonide; 3, budesonide plus formoterol;
 #'   4, fluticasone; 5, fluticasone plus salmeterol; 6, formoterol; 7, salmeterol; and 8, tiotropium.
 #'
 #' @docType data
