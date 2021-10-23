48c3ebdc0b4adb5147d872ffd4f55142807d6ad8
LoukiaSpin <spineli.loukia@mh-hannover.de>
2021-10-23 21:49:40 +0200

    Update vignettes

--- R/globals.R
+++ R/globals.R
@@ -66,3 +66,4 @@ utils::globalVariables(c("active",
 Sys.setenv(`_R_S3_METHOD_REGISTRATION_NOTE_OVERWRITES_` = "false")
 Sys.setenv(JAGS_HOME = "C:/Program Files/JAGS/JAGS-4.2.0")
 Sys.setenv(JAGS_ROOT = "C:/Program Files/JAGS/JAGS-4.2.0")
+
