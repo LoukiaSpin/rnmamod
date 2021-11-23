a1be1675e66073d8f2a85c2ece17739fe4b7873f
LoukiaSpin <spineli.loukia@mh-hannover.de>
2021-11-23 13:26:50 +0100

    Add minor correction

--- R/run.model_function.R
+++ R/run.model_function.R
@@ -385,29 +385,29 @@ run_model <- function(data,
   n_thin <- ifelse(missing(n_thin), 1, n_thin)
 
   # Data in list format for R2jags
-  data_jag <- suppressWarnings({
-    list("m" = item$m,
-         "N" = item$N,
-         "t" = item$t,
-         "na" = item$na,
-         "nt" = item$nt,
-         "ns" = item$ns,
-         "ref" = item$ref,
-         "I" = item$I,
-         "M" = ifelse(!is.na(item$m), mean_misspar, NA),
-         "cov.phi" = 0.5 * var_misspar,
-         "var.phi" = var_misspar,
-         "meand.phi" = mean_misspar,
-         "precd.phi" = 1 / var_misspar,
-         "D" = D,
-         "heter.prior" = heterog_prior)
-  })
+  data_jag0 <- list("m" = item$m,
+                    "N" = item$N,
+                    "t" = item$t,
+                    "na" = item$na,
+                    "nt" = item$nt,
+                    "ns" = item$ns,
+                    "ref" = item$ref,
+                    "I" = item$I,
+                    "M" = ifelse(!is.na(item$m), mean_misspar, NA),
+                    "cov.phi" = 0.5 * var_misspar,
+                    "var.phi" = var_misspar,
+                    "meand.phi" = mean_misspar,
+                    "precd.phi" = 1 / var_misspar,
+                    "D" = D,
+                    "heter.prior" = heterog_prior)
 
   if (is.element(measure, c("MD", "SMD", "ROM"))) {
-    data_jag <- append(data_jag, list("y.o" = item$y0, "se.o" = item$se0))
+    data_jag0 <- append(data_jag0, list("y.o" = item$y0, "se.o" = item$se0))
   } else if (measure == "OR") {
-    data_jag <- append(data_jag, list("r" = item$r))
+    data_jag0 <- append(data_jag0, list("r" = item$r))
   }
+
+  data_jag <- suppressWarnings({data_jag0})
 
   param_jags <- c("delta",
                   "EM",
