f9a3483992b4ddcead958460810b928b4618adb2
LoukiaSpin <spineli.loukia@mh-hannover.de>
2024-01-05 17:37:19 +0100

    Update dendro_heatmap function

--- R/dendro.heatmap_function.R
+++ R/dendro.heatmap_function.R
@@ -3,35 +3,36 @@
 #'
 #' @description
 #'   \code{dendro_heatmap} creates a dendrogram alongside the heatmap of
-#'   Gower dissimilarities among the trials in the network for a specific 
-#'   linkage method and number of clusters. T
+#'   Gower dissimilarities among the trials in the network for a specific
+#'   linkage method and number of clusters.
 #'
 #' @param input An object of S3 class \code{\link{comp_clustering}}. See 'Value'
 #'   in \code{\link{comp_clustering}}.
-#' @param label_size A positive integer for the font size of the heatmap 
-#'   elements. \code{label_size} determines the size argument found in the 
+#' @param label_size A positive integer for the font size of the heatmap
+#'   elements. \code{label_size} determines the size argument found in the
 #'   geom's aesthetic properties in the R-package
 #'   \href{https://CRAN.R-project.org/package=ggplot2}{ggplot2}.
-#' @param axis_text_size A positive integer for the font size of row and column 
+#' @param axis_text_size A positive integer for the font size of row and column
 #'   names of the heatmap. \code{axis_text_size} determines the axis.text
-#'   argument found in the theme's properties in the R-package 
+#'   argument found in the theme's properties in the R-package
 #'   \href{https://CRAN.R-project.org/package=ggplot2}{ggplot2}.
 #'
 #' @return
 #'   \code{dendro_heatmap} uses the \code{\link[heatmaply:heatmaply]{heatmaply}}
-#'   function of the R-package
+#'    function of the R-package
 #'   \href{https://CRAN.R-project.org/package=heatmaply}{heatmaply} to create a
 #'   cluster heatmap for a selected linkage method and number of clusters. The
 #'   function uses different colours to indicate the clusters directly on the
-#'   dendrogram. The names of the leaves refer to the trials and corresponding 
-#'   pairwise comparison.
+#'   dendrogram, specified using the R-package
+#'   \href{https://CRAN.R-project.org/package=dendextend}{dendextend}. The names
+#'   of the leaves refer to the trials and corresponding pairwise comparison.
 #'
 #'  @details
-#'    The function inherits the linkage method and number of optimal clusters by 
+#'    The function inherits the linkage method and number of optimal clusters by
 #'    the \code{\link{comp_clustering}} function.
 #'
 #'    Remember: when using the \code{\link{comp_clustering}} function, inspect
-#'    the average silhouette width for a wide range of clusters to decide on the 
+#'    the average silhouette width for a wide range of clusters to decide on the
 #'    optimal number of clusters.
 #'
 #' @author {Loukia M. Spineli}
@@ -59,7 +60,7 @@ dendro_heatmap <- function (input,
 
   ## 'Optimal' linkage method (based on the cophenetic coefficient)
   optimal_link <- if (is.null(input$Optimal_link)) {
-    stop("The function can be used *only* for hierarchical clustering.", 
+    stop("The function can be used *only* for hierarchical clustering.",
          call. = FALSE)
   } else {
     input$Optimal_link
@@ -70,29 +71,45 @@ dendro_heatmap <- function (input,
   optimal_clusters <- length(unique(input$Cluster_comp[, 2]))
 
 
+  ## Prepare a data-frame with the 'study-comparison', 'cluster' and 'silhouette width'
+  cluster_comp <- cbind(rownames(input$Trials_diss_table), input$Cluster_comp[, 2:3])
+  colnames(cluster_comp)[1] <- "study"
+
+
+  ## Get the labels after ordering
+  ordered_labels <-
+    labels(reorder(as.dendrogram(hclust(diss, optimal_link)), cluster_comp[, 2]))
+
+
+  ## Order the cluster and corresponding colour
+  ordered_clust_col <-
+    cluster_comp[order(match(cluster_comp[, 1], ordered_labels), decreasing = FALSE), ]
+
+  ## Now colour the branches with the correct colour
+  row_dend <-
+    reorder(as.dendrogram(hclust(as.dist(input$Trials_diss_table), optimal_link)),
+            cluster_comp[, 2]) %>%
+    set("branches_k_color",
+        value = unique(hue_pal()(optimal_clusters)[ordered_clust_col[, 2]]),
+        k = optimal_clusters)
+
+
   ## Create heatmap with dendrogram and coloured clusters
   dendro_heatmap <-
     heatmaply(as.matrix(diss),
               cellnote = round(as.matrix(diss), 2),
+              seriate = "none",
               xlab = " ",
               ylab = " ",
               scale = "none",
-              k_col = optimal_clusters,
-              k_row = optimal_clusters,
-              #Rowv = hclust(diss, optimal_link),
-              #Colv = hclust(diss, optimal_link),
-              Rowv = reorder(reorder(as.dendrogram(hclust(diss, optimal_link)),
-                                     input$Cluster_comp[, 3]), 
-                             input$Cluster_comp[, 2]),
-              Colv = reorder(reorder(as.dendrogram(hclust(diss, optimal_link)),
-                                     input$Cluster_comp[, 3]), 
-                             input$Cluster_comp[, 2]),
+              Rowv = row_dend,
+              Colv = row_dend,
               scale_fill_gradient_fun =
                 scale_fill_gradient2(name = " ",
                                      low = "white",
                                      high = "red",
                                      na.value = "grey90",
-                                     midpoint = 0.0, 
+                                     midpoint = 0.0,
                                      limits = c(0, 1)),
               cellnote_size = label_size ,
               fontsize_row = axis_text_size,
