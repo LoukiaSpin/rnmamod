5895fe6a2d09bd0ffad13ad14357677241eef065
LoukiaSpin <spineli.loukia@mh-hannover.de>
2024-09-05 16:10:16 +0200

    Update rainbow_similarities and weight_defined functions

--- R/rainbow.similarities_function.R
+++ R/rainbow.similarities_function.R
@@ -19,13 +19,16 @@
 #'   found in the geom's aesthetic properties in the R-package
 #'   \href{https://CRAN.R-project.org/package=ggplot2}{ggplot2}.
 #'
-#' @return A horizintal bar plot illustrating the range of Gower's similarity
+#' @return A horizontal bar plot illustrating the range of Gower's similarity
 #' values for each study using shades of red and green to indicate low and
-#' substantial similarity, respectively: the darker the red, the lower the
-#' similarity (corresponding to values close to 0), whilst the darker the green,
-#' the higher the similarity (corresponding to values close to 1). The study
-#' names appear on the y-axis in the order they appear in \code{results}
-#' and the similarity values appear on the x-axis.
+#' substantial between-comparisons similarity, respectively: the darker the red,
+#' the lower the between-comparisons similarity (corresponding to values close
+#' to 0), whilst the darker the green, the higher the between-comparisons
+#' similarity (corresponding to values close to 1). The study names appear on
+#' the y-axis in the order they appear in \code{results} and the similarity
+#' values appear on the x-axis. Red and blue points refer to the (average)
+#' within-comparison and between-comparisons similarity, respectively, for each
+#' study.
 #'
 #' @details
 #' The range of Gower's similarity values for each study result from calculating
@@ -81,21 +84,59 @@ rainbow_similarities <- function(results,
 
   ## Distinguish between two-arm and multi-arm studies
   # Get the unique study ID (remove the compared treatments)
-  index <- sub("\\(.*", "", gsub('.{3}$', '', rownames(weight)))
+  index <- gsub("^\\s+|\\s+$", "",
+                sub("\\(.*", "", gsub('.{3}$', '', rownames(weight))))
 
   # Split dataset by 'index'
   split_multi_arms <- split(weight, factor(index, levels = unique(index)))
 
+  # Get the comparison for each study
+  comp_index <- gsub("^\\s+|\\s+$", "",
+                     substring(rownames(weight), nchar(rownames(weight)) - 3))
+
+  # Split 'weight' further by 'rownames(weight)'
+  split_comp <- split(weight, factor(rownames(weight), levels = unique(rownames(weight))))
+
+  # Split 'split_comp' further by 'comp_index'
+  split_study_comp <- lapply(split_comp, function(x) split(x, comp_index))
+
+  # Set of within-comparison similarities per study
+  within_set <-
+    lapply(1:length(split_study_comp),
+           function(x)
+             unlist(split_study_comp[[x]][is.element(names(split_study_comp[[x]]),
+                                                     comp_index[x])]))
+
+  # Within-comparison similarity per study
+  within_comp <-
+    sqrt(unlist(lapply(within_set, function(x) {sum(na.omit(x)^2) /
+        (length(x) - sum(is.na(x)))})))
+
+  # Set of between-comparisons similarities per study
+  between_set <-
+    lapply(1:length(split_study_comp),
+           function(x)
+             unlist(split_study_comp[[x]][!is.element(names(split_study_comp[[x]]),
+                                                      comp_index[x])]))
+
+  # Between-comparisons similarity per study
+  between_comp <-
+    sqrt(unlist(lapply(between_set, function(x) {sum(x^2) / length(x)})))
+
 
   ## Transform the weights
   # Get the lower bound for the uniform distribution
-  lower <- unlist(lapply(split_multi_arms, function(x) min(x, na.rm = TRUE)))
+  lower <- aggregate(unlist(lapply(between_set, function(x) min(x))),
+                     by = list(factor(index, levels = index)), min)[, 2]
+
+
 
   # Get the upper bound for the uniform distribution
-  upper <- unlist(lapply(split_multi_arms, function(x) max(x, na.rm = TRUE)))
+  upper <- aggregate(unlist(lapply(between_set, function(x) max(x))),
+                     by = list(factor(index, levels = index)), min)[, 2]
 
   # Bring together
-  tranform_result <- data.frame(lower, upper)
+  tranform_result <- data.frame(lower, upper); rownames(tranform_result) <- index
 
 
   ## Create a sequence of values to define the colour shades
@@ -132,16 +173,16 @@ rainbow_similarities <- function(results,
   data_raw$min <- data_raw$value; data_raw$max <- data_raw$value
 
 
-  ## Get the root mean square (rms) values
-  # Prepare dataset
-  rms_res <-
-    sqrt(unlist(lapply(split_multi_arms, function(x) {sum(na.omit(x)^2) /
-        (length(x) - sum(is.na(x)))})))
+  ## Prepare extra dataset for the 'within_comp' and 'between_comp'
+  # Include 'within-comparison' similarities
+  data_rms_within <- data.frame(var = unique(names(split_multi_arms)),
+                                melt(within_comp),
+                                study_id = 1:length(within_comp))
 
-  # Bring together
-  data_rms <- data.frame(var = names(rms_res),
-                         melt(rms_res),
-                         study_id = 1:length(split_multi_arms))
+  # Include 'betweenn-comparison' similarities
+  data_rms_between <- data.frame(var = unique(names(split_multi_arms)),
+                                 melt(between_comp),
+                                 study_id = 1:length(between_comp))
 
 
   ## Get the plot! :-)
@@ -167,34 +208,60 @@ rainbow_similarities <- function(results,
     scale_y_reverse(breaks = 1:dim(tranform_result)[1],
                     labels = rownames(tranform_result),
                     expand = c(0, 0)) +
-    geom_point(data = data_rms,
+    geom_point(data = data_rms_within,
                aes(x = value,
-                   y = study_id),
+                   y = study_id,
+                   fill = "Within-comparison"),
+               color = "red",
                size = 3.0,
                shape = "diamond",
                inherit.aes = FALSE) +
-    geom_text(data = data_rms,
+    geom_point(data = data_rms_between,
+               aes(x = value,
+                   y = study_id,
+                   fill = "Between-comparisons"),
+               color = "blue",
+               size = 3.0,
+               shape = "diamond",
+               inherit.aes = FALSE) +
+    geom_text(data = data_rms_within,
               aes(x = value,
                   y = study_id,
-                  label = sprintf("%.2f", value)), # sprintf("%.1f", value * 100)
+                  label = sprintf("%.2f", value)),
+              size = label_size,
+              vjust = 0.5, # -0.85
+              hjust = 1.25,
+              inherit.aes = FALSE) +
+    geom_text(data = data_rms_between,
+              aes(x = value,
+                  y = study_id,
+                  label = sprintf("%.2f", value)),
               size = label_size,
               vjust = 0.5, # -0.85
               hjust = 1.25,
               inherit.aes = FALSE) +
     labs(x = "Range of Gower's similarity values",
-         y = "") +
+         y = "",
+         fill = "Similarity type") +
     scale_x_continuous(limits = c(0, 1),
                        breaks = seq(0, 1, 0.1),
                        labels = sprintf("%.2f", seq(0, 1, 0.1)),
                        #labels = scales::percent,
-                       expand = c(0.01,0)) +
+                       expand = c(0.01, 0)) +
+    scale_fill_manual(values = c("Within-comparison" = "red",
+                                 "Between-comparisons" = "blue")) +
+    guides(colour = "none") +
     theme_classic() +
     theme(axis.title = element_text(size = axis_title_size,
                                     face = "bold",
                                     colour = "black"),
           axis.text = element_text(size = axis_text_size),
           panel.border = element_blank(),
-          legend.position = "none")
+          legend.position = "bottom",
+          legend.title = element_text(size = axis_title_size,
+                                      face = "bold",
+                                      colour = "black"),
+          legend.text = element_text(size = axis_text_size))
 
   return(p1)
 }
--- R/weight.defined_function.R
+++ R/weight.defined_function.R
@@ -1,125 +1,155 @@
-#' Preparing the study weights based on Gower's dissimilarities 
+#' Preparing the study weights based on Gower's dissimilarities
 #' (Transitivity evaluation)
-#' 
+#'
 #' @description
-#' Using the Gower's dissimilarities to prepare the study weights in a network 
-#' of interventions to be used by the \code{\link{run_model}} function in the 
-#' context of the transitivity evaluation as a sensitivity analysis. 
-#' 
+#' Using the Gower's dissimilarities to prepare the study weights in a network
+#' of interventions to be used by the \code{\link{run_model}} function in the
+#' context of the transitivity evaluation as a sensitivity analysis.
+#'
 #' @param diss_res A list of two elements with the following order:
 #'   1) an object of S3 class 'comp_clustering' (\code{\link{comp_clustering}}),
-#'   and 2) a character with values \code{"uniform"}, or \code{"rms"} to define 
-#'   the usage of the similarities (see 'Details') as being sampled from a 
-#'   uniform distribution, or being fixed to their root mean square. 
-#'   See 'Details'.
-#'   
+#'   and 2) a character with values \code{"uniform"}, or \code{"rms"} to define
+#'   the usage of the similarities (see 'Details') as being sampled from a
+#'   uniform distribution, or being a ratio of their between-comparisons
+#'   similarity and total similarity based on root mean square. See 'Details'.
+#'
 #' @return A list of the following two elements:
 #'   \item{weights}{A vector of study weights if \code{"rms"} has been specified
 #'   or a matrix with the minimum and maximum similarity values for each study
 #'   if \code{"uniform"} has been specified.}
-#'   \item{type}{A character indicating the weight type considered: 
+#'   \item{type}{A character indicating the weight type considered:
 #'   \code{"rms"} or \code{"uniform"}.}
-#'   
+#'
 #' @details
-#' The function receives the matrix \code{Trials_diss_table} found in the 
+#' The function receives the matrix \code{Trials_diss_table} found in the
 #' results of \code{\link{comp_clustering}}. This matrix contains the Gower's
-#' dissimilarities of all study pairs in the network for a specific set of 
+#' dissimilarities of all study pairs in the network for a specific set of
 #' clinical and methodological characteristics that act as effect modifiers.
-#' Gower's dissimilarities take values from 0 to 1 (or 0 to 100%), with 0 and 
-#' 1 (or 100%) implying perfect similarity and perfect dissimilarity, 
-#' respectively. Hence, subtracting each value from 1 yields the similarities of 
-#' all study pairs in the network. 
-#' 
+#' Gower's dissimilarities take values from 0 to 1, with 0 and 1 implying
+#' perfect similarity and perfect dissimilarity, respectively. Hence,
+#' subtracting each value from 1 yields the similarities of all study pairs in
+#' the network.
+#'
 #' For each study, the Gower's similarities can be either transformed into the
-#' root mean square or restricted to the minimum and maximum values. This 
-#' depends on the second element specified in the argument \code{diss_res}. 
-#' When the \code{"uniform"} choice has been specified in the second element 
+#' root mean square or restricted to the minimum and maximum values. This
+#' depends on the second element specified in the argument \code{diss_res}.
+#' When the \code{"uniform"} choice has been specified in the second element
 #' of the argument \code{diss_res}, the function checks whether the bounds are
-#' the same value. If this is the case for at least one study, the uniform 
-#' distribution cannot be defined and, hence, the function returns the root mean 
+#' the same value. If this is the case for at least one study, the uniform
+#' distribution cannot be defined and, hence, the function returns the root mean
 #' square value (\code{"rms"}) for all studies.
-#' 
+#'
 #' \code{weight_defined} can be used in \code{\link{run_model}} via the argument
 #' \code{adjust_wgt}. See 'Details' in \code{\link{run_model}}.
-#' 
+#'
 #' @author {Loukia M. Spineli}
-#' 
+#'
 #' @seealso \code{\link{comp_clustering}}, \code{\link{run_model}}
-#' 
+#'
 #' @references
 #' Gower J. General Coefficient of Similarity and Some of Its Properties.
 #' \emph{Biometrics} 1971;\bold{27}(4):857--71.
 #' doi: 10.2307/2528823
-#' 
+#'
 #' @export
 weight_defined <- function (diss_res) {
-  
+
   ## Check default
   if (inherits(diss_res[[1]], "comp_clustering") == FALSE) {
-    stop("The first argument must be an object of S3 class 'comp_clustering'", 
+    stop("The first argument must be an object of S3 class 'comp_clustering'",
          call. = FALSE)
   }
   transf <- if (!is.element(diss_res[[2]], c("rms", "uniform"))) {
-    stop("Insert one of the following: 'rms', or 'uniform'.", 
+    stop("Insert one of the following: 'rms', or 'uniform'.",
          call. = FALSE)
   } else {
     diss_res[[2]]
   }
-  
+
   ## Consider the matrix of study dissimilarities
   diss <- diss_res[[1]]$Trials_diss_table
-  
+
   ## Copy-paste the lower off-diagonal elements to the corresponding upper
   diss[upper.tri(diss)] <- t(diss)[upper.tri(diss)]
   diss
-  
+
   ## Turn into weights (1 - diss)
   weight <- 1 - diss
-  
+
   ## Turn diagonal into NA
   diag(weight) <- NA
-  
+
   ## Distinguish between two-arm and multi-arm studies
   # Get the unique study ID
-  index <- sub("\\(.*", "", gsub('.{3}$', '', rownames(weight)))
-  
-  # Split dataset by 'index'
-  split_multi_arms <- split(weight, factor(index, levels = unique(index)))
-  
+  index <- gsub("^\\s+|\\s+$", "",
+                sub("\\(.*", "", gsub('.{3}$', '', rownames(weight))))
+
+  # Get the comparison for each study
+  comp_index <- gsub("^\\s+|\\s+$", "",
+                     substring(rownames(weight), nchar(rownames(weight)) - 3))
+
+  # Split 'weight' further by 'rownames(weight)'
+  split_comp <- split(weight, factor(rownames(weight), levels = unique(rownames(weight))))
+
+  # Split 'split_comp' further by 'comp_index'
+  split_study_comp <- lapply(split_comp, function(x) split(x, comp_index))
+
+  # Set of within-comparison similarities per study
+  within_set <-
+    lapply(1:length(split_study_comp),
+           function(x)
+             unlist(split_study_comp[[x]][is.element(names(split_study_comp[[x]]),
+                                                     comp_index[x])]))
+
+  # Within-comparison similarity per study
+  within_comp <-
+    sqrt(unlist(lapply(within_set, function(x) {sum(na.omit(x)^2) /
+        (length(x) - sum(is.na(x)))})))
+
+  # Set of between-comparisons similarities per study
+  between_set <-
+    lapply(1:length(split_study_comp),
+           function(x)
+             unlist(split_study_comp[[x]][!is.element(names(split_study_comp[[x]]),
+                                                      comp_index[x])]))
+
+  # Between-comparisons similarity per study
+  between_comp <-
+    sqrt(unlist(lapply(between_set, function(x) {sum(x^2) / length(x)})))
+
+
   ## Transform the weights: sampled from a selected distribution or fixed to rms
   tranform_result <-
     if (transf == "uniform") {    # Uniform distribution
-    # Lower bound
-    lower <- unlist(lapply(split_multi_arms, function(x) min(x, na.rm = TRUE)))
-    
-    # upper bound
-    upper <- unlist(lapply(split_multi_arms, function(x) max(x, na.rm = TRUE)))
-    
+    # Lower bound (Return the minimum for multi-arm studies)
+    lower <- aggregate(unlist(lapply(between_set, function(x) min(x))), by = list(index), min)[, 2]
+
+    # upper bound (Return the minimum for multi-arm studies)
+    upper <- aggregate(unlist(lapply(between_set, function(x) max(x))), by = list(index), min)[, 2]
+
     data.frame(lower, upper)
-  } else if (transf == "rms") { # Average
-    sqrt(unlist(lapply(split_multi_arms, function(x) {sum(na.omit(x)^2) / 
-        (length(x) - sum(is.na(x)))})))
+  } else if (transf == "rms") { # Return the minimum for multi-arm studies
+    aggregate(between_comp / (within_comp + between_comp), by = list(index), min)[, 2]
   }
-  
-  
+
+
   ## Check whether the uniform distribution is undefined
   with_problem <- if (transf == "uniform") {
     any(apply(tranform_result, 1, diff) == 0)
   } else if (transf == "rms") {
     FALSE
-  }  
+  }
 
-  
+
   ## When the uniform distribution is undefined, switch to 'rms'
   proper_tranform <- if (all(transf == "uniform" & with_problem == TRUE) == TRUE) {
     message("Undefined uniform distribution. Root mean square was used instead.")
-    sqrt(unlist(lapply(split_multi_arms, function(x) {sum(na.omit(x)^2) / 
-        (length(x) - sum(is.na(x)))})))
+    aggregate(between_comp / (within_comp + between_comp), by = list(index), min)[, 2]
   } else {
     tranform_result
   }
 
-  
+
   ## Consider the 'proper' transformation: 'uniform', or 'rms'
   transf_new <- if (with_problem == TRUE) {
     "rms"
